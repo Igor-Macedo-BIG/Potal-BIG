993548ae81ec29142d0643a23d93e280bd934074
Igor Macedo <igorwillian.macedo@gmail.com>
2025-12-01 15:40:55 -0300

    fix: Change to next.config.mjs for Vercel compatibility

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-﻿import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  // Não precisa configurar srcDir, Next.js detecta automaticamente
-  reactStrictMode: true,
-};
-
-export default nextConfig;
